2ae8f85d6ed1254419f21d2e6d0ee5d23669d7e8
JerryLin <jerry.lin@titansoft.com.sg>
2020-07-19 14:04:15 +0800

    refactor test project

--- OrderStringWithNumber/TestOrder/UnitTest1.cs
+++ OrderStringWithNumber/TestOrder/UnitTest1.cs
@@ -13,17 +13,25 @@ namespace Tests
         [TestCase("", "")]
         public void TestStringEmpty(string words, string answer)
         {
-            OrderString orderString = new OrderString();
-            Assert.AreEqual(orderString.Order(words), answer);
+            TestWithWordsAndAnswer(words, answer);
         }
         [TestCase("w1", "w1")]
         public void TestOneWords(string words, string answer)
         {
-            OrderString orderString = new OrderString();
-            Assert.AreEqual(orderString.Order(words), answer);
+            TestWithWordsAndAnswer(words, answer);
         }
         [TestCase("is2 Thi1s", "Thi1s is2")]
         public void TestTwoWords(string words, string answer)
+        {
+            TestWithWordsAndAnswer(words, answer);
+        }
+        [TestCase("is2 Thi1s T4est 3a", "Thi1s is2 3a T4est")]
+        public void TestFourWords(string words, string answer)
+        {
+            TestWithWordsAndAnswer(words, answer);
+        }
+
+        private void TestWithWordsAndAnswer(string words, string answer)
         {
             OrderString orderString = new OrderString();
             Assert.AreEqual(orderString.Order(words), answer);
